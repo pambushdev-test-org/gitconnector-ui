1774516263a174be165a21fdc5d8580b1d3ba959
Paris Ambush <paris.ambush@gmail.com>
2022-01-23 00:12:47 -0800

    Use g1-micro for cluster machine type

--- .terraform/cluster/main.tf
+++ .terraform/cluster/main.tf
@@ -14,16 +14,14 @@ resource "google_container_cluster" "primary" {
   initial_node_count = 1
 
   master_auth {
-    #username = ""
-    #password = ""
-
     client_certificate_config {
       issue_client_certificate = false
     }
   }
 
   node_config {
-    #machine_type = var.machine_type
+    preemptible  = true
+    machine_type = var.machine_type
     oauth_scopes = [
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
